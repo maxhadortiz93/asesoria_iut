fa064c3cb037cb160b7d51f3c48e5c494bd8a237
machadojesus <admin@amariyo.cash>
2025-10-26 08:51:28 -0400

    Arreglar linkeo de estilos y Blade layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,15 @@
 <!doctype html>
 <html>
-  <head>
+<head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    @vite('resources/css/app.css')
-  </head>
-  <body>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  </body>
+    <title>Inventario</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    @include('layouts.head')
+    <main class="max-w-7xl mx-auto px-4 py-8">
+        {{ $slot ?? '' }}
+    </main>
+</body>
 </html>
